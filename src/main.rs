c77576a66e5869c83f822e06be1f475f32124758
Michael <zarathir@proton.me>
2022-10-18 22:14:41 +0200

    read values from config

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use anyhow::Result;
 use lapce_plugin::{
     psp_types::{
-        lsp_types::{request::Initialize, DocumentFilter, DocumentSelector, InitializeParams, Url},
+        lsp_types::{
+            request::Initialize, DocumentFilter, DocumentSelector, InitializeParams,
+            Url,
+        },
         Request,
     },
     register_plugin, LapcePlugin, PLUGIN_RPC,
@@ -24,17 +27,10 @@ fn initialize(params: InitializeParams) -> Result<()> {
     }];
     let mut server_args = vec![];
 
-    server_args.push("language-server".to_owned());
-    server_args.push("--lsp".to_owned());
-
-    // Check for user specified LSP server path
-    // ```
-    // [lapce-plugin-name.lsp]
-    // serverPath = "[path or filename]"
-    // serverArgs = ["--arg1", "--arg2"]
-    // ```
     if let Some(options) = params.initialization_options.as_ref() {
-        if let Some(lsp) = options.get("lsp") {
+        if let Some(lsp) = options.get("volt") {
+            server_args.push(format!("language-server"));
+
             if let Some(args) = lsp.get("serverArgs") {
                 if let Some(args) = args.as_array() {
                     if !args.is_empty() {
@@ -50,31 +46,23 @@ fn initialize(params: InitializeParams) -> Result<()> {
 
             if let Some(server_path) = lsp.get("serverPath") {
                 if let Some(server_path) = server_path.as_str() {
-                    if !server_path.is_empty() {
-                        let server_uri = Url::parse(&format!("urn:{}", server_path))?;
-                        PLUGIN_RPC.start_lsp(
-                            server_uri,
-                            server_args,
-                            document_selector,
-                            params.initialization_options,
-                        );
-                        return Ok(());
-                    }
+                    let server_uri = if !server_path.is_empty() {
+                        Url::parse(&format!("urn:{}", server_path))?
+                    } else {
+                        Url::parse(&format!("urn:dart"))?
+                    };
+
+                    PLUGIN_RPC.start_lsp(
+                        server_uri,
+                        server_args,
+                        document_selector,
+                        params.initialization_options,
+                    );
+                    return Ok(());
                 }
             }
         }
     }
-
-    let server_path = Url::parse(&format!("urn:dart"))?;
-
-    // Available language IDs
-    // https://github.com/lapce/lapce/blob/HEAD/lapce-proxy/src/buffer.rs#L173
-    PLUGIN_RPC.start_lsp(
-        server_path,
-        server_args,
-        document_selector,
-        params.initialization_options,
-    );
 
     Ok(())
 }
@@ -85,7 +73,9 @@ impl LapcePlugin for State {
         match method.as_str() {
             Initialize::METHOD => {
                 let params: InitializeParams = serde_json::from_value(params).unwrap();
-                let _ = initialize(params);
+                if let Err(e) = initialize(params) {
+                    PLUGIN_RPC.stderr(&format!("plugin returned with error: {e}"))
+                }
             }
             _ => {}
         }
